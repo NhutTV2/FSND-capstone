132adbaa6cc6f1410acb87ff5fd7ffb9c1cb4000
bbs3.nhuttv <bbs3.nhuttv@techcombank.com.vn>
2023-05-13 11:46:31 +0700

    Fix bug deploy Working outside of application context

--- database/models.py
+++ database/models.py
@@ -19,8 +19,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
-    with app.app_context():
-        db.init_app(app)
+    db.init_app(app)
     db.create_all()
 
 
